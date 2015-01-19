a6f90079de3ccc55f0e74e3bbe400017f8d00592
Marko Grešak <gresak.marko@gmail.com>
2015-01-19 01:20:06 +0100

    update(binary-server): SSL server socket (wss)

--- modules/binary-server.js
+++ modules/binary-server.js
@@ -1,14 +1,46 @@
-var BinaryServer = require('binaryjs').BinaryServer,
+var https = require('https'),
+    BinaryServer = require('binaryjs').BinaryServer,
     wav = require('wav'),
-    fm = require('./file-manager.js');
+    fm = require('./file-manager.js'),
+    fs = require('fs');
+
+
+// Read and parse config file, path should be set in app.js.
+var sslConfigString = fs.readFileSync(global.sslConfigFile).toString();
+var sslConfig = JSON.parse(sslConfigString);
+
+// Throw error if options are not set correctly.
+if(!sslConfig || !sslConfig.key || !sslConfig.cert) {
+  throw Error('Failed to read SSL options.');
+}
+
+// Read SSL files using values from config.
+var sslOptions = {
+  key: fs.readFileSync(sslConfig.key),
+  cert: fs.readFileSync(sslConfig.cert)
+};
 
 /**
- * Initialize new BinaryServer on port `global.binaryServerPort`.
+ * Https server using `sslOptions` from sslConfig.json,
+ * on port `global.binaryServerPort`.
+ *
+ * @type {HttpsServer}
+ */
+var httpsServer = https.createServer(sslOptions, function(req, res) {
+  'use strict';
+  // GET request, send head 200 and a generic message.
+  res.writeHead(200);
+  res.end('Web Socket Server\n');
+}).listen(global.binaryServerPort);
+
+/**
+ * Initialize new BinaryServer using `httpsServer`.
  *
  * @type {BinaryServer}
  */
-var binaryServer = new BinaryServer({ port: global.binaryServerPort });
+var binaryServer = new BinaryServer({server: httpsServer});
 
+// Event on new client conenction.
 binaryServer.on('connection', function(client) {
   'use strict';
 
@@ -25,11 +57,13 @@ binaryServer.on('connection', function(client) {
     bitDepth: 16
   });
 
+  // Event on data recieved.
   client.on('stream', function(stream) {
     console.log('new stream: ' + filePath);
 
     stream.pipe(fileWriter);
 
+    // Event on stream closed.
     stream.on('end', function() {
       fileWriter.end();
       console.log('--- end stream: ' + filePath);
